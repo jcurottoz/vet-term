10ddf58e11c41a7c198dbccd58a670d50a1ca1e5
jcurotto <134725185+jcurottoz@users.noreply.github.com>
2026-08-20 14:53:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1031,43 +1031,42 @@ if pagina == "🏠 Inicio":
 
     with col_titulo:
 
-        st.markdown(
-            """
-            <div style="
-                text-align:center;
-                padding:20px 0;
+    st.markdown(
+        """
+        <div style="
+            text-align: center;
+            padding: 20px 0;
+        ">
+
+            <h1 style="
+                margin: 0;
+                color: #8B1E2D;
+                font-size: 42px;
             ">
+                🐾 VET-TERM
+            </h1>
 
-                <h1 style="
-                    margin:0;
-                    color:#8B1E2D;
-                    font-size:42px;
-                ">
-                    🐾 VET-TERM
-                </h1>
+            <h3 style="
+                margin: 12px 0 0 0;
+                color: #333333;
+                font-weight: 500;
+            ">
+                Aprende terminología médico-veterinaria jugando
+            </h3>
 
-                <h3 style="
-                    margin:12px 0 0 0;
-                    color:#333333;
-                    font-weight:500;
-                ">
-                    Aprende terminología médico-veterinaria jugando
-                </h3>
+            <p style="
+                margin-top: 14px;
+                color: #555555;
+                font-size: 16px;
+            ">
+                Facultad de Medicina Veterinaria y Zootecnia<br>
+                Universidad Nacional San Luis Gonzaga
+            </p>
 
-                <p style="
-                    margin-top:14px;
-                    color:#555555;
-                    font-size:16px;
-                ">
-                    Facultad de Medicina Veterinaria y Zootecnia<br>
-                    Universidad Nacional San Luis Gonzaga
-                </p>
-
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
     # -----------------------------------------------------
     # LOGO FACULTAD
     # -----------------------------------------------------
